823ef216adddf339088dbd0eaa2d566ebb6dc999
Thi Chieu <thi@bespokify.com>
2021-09-06 16:15:44 +0700

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': 'off',
-    'semi': 'off',
+    semi: 'off',
     'space-before-function-paren': 'off'
   },
 }
